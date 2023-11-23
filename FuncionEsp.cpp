f6232d735ae675ee868efa60712a5fa95b2ef8e1
Jiachen <146822199+Jyejii@users.noreply.github.com>
2023-11-23 13:01:40 +0100

    Ej4

---
+++ FuncionEsp.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <string>
+
+int main() {
+    std::string sentence = "El lenguaje C++ es poderoso";
+    std::string sub_str = "C++";
+
+    size_t pos = sentence.find(sub_str);
+
+
+    if (pos != std::string::npos) {
+        std::string new_sentence = sentence;
+        new_sentence.replace(pos, sub_str.length(), "Python");
+        std::cout << "Cadena resultante: " << new_sentence << std::endl;
+    } else {
+        std::cout << "Subcadena no encontrada" << std::endl;
+    }
+
+    return 0;
+}
+
