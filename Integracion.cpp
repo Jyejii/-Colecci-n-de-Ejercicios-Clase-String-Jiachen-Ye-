ca927800cc94cdc7ca60b47de24ce47c6fecaf07
Jiachen <146822199+Jyejii@users.noreply.github.com>
2023-11-23 12:47:47 +0100

    Ejercicio 3

---
+++ Integracion.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <string>
+
+
+int main() {
+    std::string str1="Hola ";
+    std::string str2="Mundo ";
+    std::string str3="Como ";
+    std::string str4="Estan ?";
+    std::string resultado=str1+str2+str3+str4;
+    std::cout<<"Cadena resultante:"<<resultado  <<std::endl;
+
+    return 0;
+}
